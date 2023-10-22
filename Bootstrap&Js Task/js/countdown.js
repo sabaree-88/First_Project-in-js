8f4a5e2bba6f8f33c3db4faff3801bcb91d3ea99
Sabareesh <88962485+sabaree-88@users.noreply.github.com>
2023-10-22 22:32:54 +0530

    Add files via upload

--- Bootstrap&Js Task/js/countdown.js
+++ Bootstrap&Js Task/js/countdown.js
@@ -1,53 +1,67 @@
-var start = document.getElementById("start");
-var reset = document.getElementById("reset");
-var stop = document.getElementById("stop");
+let countdown;
+let totalTime = 0;
+let currentTime = 0;
 
-var hr = document.getElementById("hour");
-var min = document.getElementById("min");
-var sec = document.getElementById("sec");
+const hoursInput = document.getElementById("hours");
+const minutesInput = document.getElementById("minutes");
+const secondsInput = document.getElementById("seconds");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const resetButton = document.getElementById("resetButton");
+const timerDisplay = document.getElementById("timer");
+const progressBar = document.getElementById("progress-bar");
 
-var startTimer = null;
+function startTimer() {
+  if (totalTime === 0) {
+    totalTime = (parseInt(hoursInput.value) * 3600) + (parseInt(minutesInput.value) * 60) + parseInt(secondsInput.value);
+    currentTime = totalTime;
+    updateTimerDisplay();
+  }
 
-function timer(){
-    // sec.value--;
-    if(hr.value == 0 && min.value == 0 && sec.value == 0){
-        hr.value = 0;
-        min.value = 0;
-        sec.value = 0;
-    }
-    else if(sec.value != 0){
-        sec.value--;
-    }
-    else if(min.value != 0 && sec.value == 0){
-        sec.value = 60;
-        min.value--;
-    }
-    else if(hr.value != 0 && min.value == 0){
-        min.value = 60;
-        hr.value--;
-    }
+  countdown = setInterval(updateTimer, 1000);
+  startButton.disabled = true;
 }
 
-function resetTimer(){
-    clearInterval(startTimer);
+function stopTimer() {
+  clearInterval(countdown);
+  startButton.disabled = false;
 }
 
-start.addEventListener('click',()=>{
-    function startInterval(){
-        startInterval = setInterval(()=>{
-            timer();
-        }, 1000);
-    }
-    startInterval();
-});
+function resetTimer() {
+  clearInterval(countdown);
+  totalTime = 0;
+  currentTime = 0;
+  hoursInput.value = "0";
+  minutesInput.value = "0";
+  secondsInput.value = "0";
+  updateTimerDisplay();
+  progressBar.style.width = "0";
+  startButton.disabled = false;
+}
 
-reset.addEventListener('click', ()=>{
-    hr.value = 0;
-    min.value = 0;
-    sec.value = 0;
-    resetTimer();
-});
+function updateTimer() {
+  if (currentTime > 0) {
+    currentTime--;
+    updateTimerDisplay();
+    updateProgressBar();
+  } else {
+    clearInterval(countdown);
+  }
+}
 
-stop.addEventListener('click',()=>{
-    clearInterval(timer(f));
-})
+function updateTimerDisplay() {
+  const hours = Math.floor(currentTime / 3600);
+  const minutes = Math.floor((currentTime % 3600) / 60);
+  const seconds = currentTime % 60;
+
+  timerDisplay.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+function updateProgressBar() {
+  const percentage = ((totalTime - currentTime) / totalTime) * 100;
+  progressBar.style.width = percentage + "%";
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);

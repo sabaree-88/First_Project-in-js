4e37a36af57d8c69073b44b6e8264f5adb79428d
Sabareesh <88962485+sabaree-88@users.noreply.github.com>
2023-10-25 15:42:15 +0530

    Add files via upload

--- Bootstrap&Js Task/js/countdown.js
+++ Bootstrap&Js Task/js/countdown.js
@@ -2,24 +2,41 @@ let countdown;
 let totalTime = 0;
 let currentTime = 0;
 
-const hoursInput = document.getElementById("hours");
-const minutesInput = document.getElementById("minutes");
-const secondsInput = document.getElementById("seconds");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
 const resetButton = document.getElementById("resetButton");
 const timerDisplay = document.getElementById("timer");
 const progressBar = document.getElementById("progress-bar");
+const percentVal = document.getElementById("percentVal");
+const msg = document.getElementById("msg");
 
+
+stopButton.disabled = true;
+resetButton.disabled = true;
 function startTimer() {
   if (totalTime === 0) {
-    totalTime = (parseInt(hoursInput.value) * 3600) + (parseInt(minutesInput.value) * 60) + parseInt(secondsInput.value);
+    totalTime = (parseInt(hours.value) * 3600) + (parseInt(minutes.value) * 60) + parseInt(seconds.value);
     currentTime = totalTime;
     updateTimerDisplay();
   }
+  if (hours.value == 0 && minutes.value == 0 && seconds.value == 0) {
+    msg.innerHTML = "Enter a value to start the countdown!";
+    startButton.disabled = false;
+
+  }
+  if (totalTime !== 0) {
+    msg.innerHTML = "";
+    startButton.disabled = true;
+  }
 
   countdown = setInterval(updateTimer, 1000);
-  startButton.disabled = true;
+  stopButton.disabled=false;
+  resetButton.disabled = false;
+
+
 }
 
 function stopTimer() {
@@ -31,11 +48,12 @@ function resetTimer() {
   clearInterval(countdown);
   totalTime = 0;
   currentTime = 0;
-  hoursInput.value = "0";
-  minutesInput.value = "0";
-  secondsInput.value = "0";
+  hours.value = "0";
+  minutes.value = "0";
+  seconds.value = "0";
   updateTimerDisplay();
   progressBar.style.width = "0";
+  percentVal.innerHTML = '';
   startButton.disabled = false;
 }
 
@@ -60,6 +78,10 @@ function updateTimerDisplay() {
 function updateProgressBar() {
   const percentage = ((totalTime - currentTime) / totalTime) * 100;
   progressBar.style.width = percentage + "%";
+  if (percentage <= 100) {
+    percentVal.innerHTML = Math.floor(percentage) + "%";
+  }
+
 }
 
 startButton.addEventListener("click", startTimer);
--- Bootstrap&Js Task/js/form_validation.js
+++ Bootstrap&Js Task/js/form_validation.js
@@ -5,17 +5,16 @@ const email = document.querySelector("#email");
 const mobileNum = document.querySelector("#phone-num");
 const password = document.querySelector("#password");
 const password2 = document.querySelector("#password2");
-const gender = document.querySelector(".radio");
+const places = document.getElementById("places-want-to-visit");
 const message = document.querySelector("#message");
-const accept = document.querySelector("#accept-to-continue");
+// const accept = document.querySelector("#accept-to-continue");
 
 
 form.addEventListener('submit', (e) => {
-    if(!validate()){
+    if (!validate()) {
         e.preventDefault();
+       
     }
-    // // e.preventDefault();
-    // validate();
 });
 
 
@@ -27,9 +26,12 @@ function validate() {
     const mobileNumberValue = mobileNum.value.trim();
     const passwordValue = password.value;
     const passwordValue2 = password2.value;
+    const placeValue = places.value;
     const messageValue = message.value;
     const checkBox = document.getElementById("accept-to-continue");
+    
     let success = true;
+    let isGenderValid = true;
 
     if (fnameValue === '') {
         success = false;
@@ -74,9 +76,9 @@ function validate() {
         success = false;
         setError(password, 'Password is requierd.');
     }
-    else if (passwordValue.length < 8) {
+    else if (!checkPassword(passwordValue)) {
         success = false;
-        setError(password, 'Password must be 8 charecter long.');
+        setError(password, 'Password must be 8 charecter long and it should contain atleast one number and special charecter and uppercase letters.');
     }
     else {
         setSucces(password)
@@ -93,6 +95,25 @@ function validate() {
     else {
         setSucces(password2);
     }
+    if(document.getElementById("male").checked != true && document.getElementById("female").checked != true && document.getElementById("others").checked != true){
+        isGenderValid = false;
+        success = false;
+        document.querySelector(".error1").innerHTML = "Gender is requierd."
+        document.querySelector(".error1").style.color="red";
+    }
+    else{
+        document.querySelector(".error1").innerHTML = ""
+        isGenderValid = true
+    }
+
+
+    if (placeValue === '') {
+        success = false;
+        setError(places, 'Choose place from this list.');
+    }
+    else {
+        setSucces(places);
+    }
     if (messageValue === '') {
         success = false;
         setError(message, 'Please enter your feedback to improve our service.');
@@ -102,16 +123,20 @@ function validate() {
     }
 
     let textCheck = document.getElementById("error")
-    if (checkBox.checked){
+    if (checkBox.checked) {
         textCheck.innerHTML = ""
     }
-    else{
+    else {
         success = false
         textCheck.innerHTML = "Please accept to continue"
     }
-
-
-    return success
+    if(isGenderValid == false){
+        return false
+    }
+    else{
+        return true, success
+    }
+    
 }
 
 function setError(element, message) {
@@ -132,16 +157,19 @@ function setSucces(element) {
     inputGroup.classList.remove('error');
 }
 
-const isValidEmail = (email) =>{
+const isValidEmail = (email) => {
     return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
 }
 
 const isvalidMobile = (mobileNum) => {
     return mobileNum.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/);
 }
 
-
+const checkPassword = (str) => {
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(str);
+}
